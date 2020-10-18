5421bebbd2896c8d3bb774d364d4829b71d09fca
Julio Cesar <jcesar_olv@outlook.com>
2020-10-18 13:20:27 -0300

    first-commit

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+
+import OrphansController from './controllers/OrphanagesController';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphansController.index);
+routes.get('/orphanages/:id', OrphansController.show);
+routes.post('/orphanages', upload.array('images'), OrphansController.create);
+
+export default routes;
